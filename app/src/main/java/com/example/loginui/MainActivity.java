2f1c0c19a6baac827324310d7e4d913926d071a5
Ashutosh Rai <55093216+Ashutosh1812@users.noreply.github.com>
2021-09-09 15:03:05 +0530

    otp page done

--- app/src/main/java/com/example/loginui/MainActivity.java
+++ app/src/main/java/com/example/loginui/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.loginui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSendOTP;
+    EditText edtMobileNO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtMobileNO = findViewById(R.id.edt_mobile_no);
+        btnSendOTP = findViewById(R.id.btn_send_otp);
+
+        btnSendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dd = edtMobileNO.getText().toString();
+                if (dd.isEmpty()){
+                    edtMobileNO.setError("Please Enter Mobile No");
+                    Toast.makeText(MainActivity.this, "Please Enter Mobile NO.", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    Intent intent = new Intent(MainActivity.this,OtpActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/loginui/OtpActivity.java
+++ app/src/main/java/com/example/loginui/OtpActivity.java
@@ -3,12 +3,41 @@ package com.example.loginui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
 
 public class OtpActivity extends AppCompatActivity {
 
+    TextView mTextField;
+    TextView textViewResendOTP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+
+        mTextField = findViewById(R.id.tv_timer);
+        textViewResendOTP = findViewById(R.id.tv_resend_otp);
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("Resend code in: " + millisUntilFinished / 1000);
+
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                mTextField.setVisibility(View.GONE);
+                textViewResendOTP.setVisibility(View.VISIBLE);
+                textViewResendOTP.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+
+        }.start();
     }
 }
